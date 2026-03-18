8a10d9c663a89ce5b6912cbe2fa136aa81c8c257
khasim <khasshaik12@gmail.com>
2026-03-18 15:28:11 +0530

    Improves ui

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,47 @@
-const main = () => {
+const refine = (e) => {
+  const parent = e.target.parentElement;
+  const child = e.target;
+  const selector = child.className || child.id;
 
+  return { parent, child, selector };
 };
 
-document.addEventListener("DOMContentLoaded", main);
+const logOut = () => {
+  console.log("logout called");
+};
+
+const deleteItem = (container, element) => {
+  console.log("deleting", { container, element, });
+};
+
+const changeStatus = (container, element) => {
+  console.log("changing status", { container, element });
+};
+
+const attachListeners = () => {
+  const body = document.querySelector("body");
+  body.addEventListener("click", (e) => {
+    const actions = {
+      logout: logOut,
+      delete: deleteItem,
+      status: changeStatus,
+    };
+
+    const attrbs = refine(e);
+
+    actions[attrbs.selector](attrbs.parent, attrbs.child);
+    console.log({ type: "clk", ...attrbs });
+  });
+  body.addEventListener("dblclick", (e) => {
+    const attrbs = refine(e);
+    console.log({ type: "dbl-clk", ...attrbs });
+  });
+  body.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      const attrbs = refine(e);
+      console.log({ type: "Enter", ...attrbs });
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", attachListeners);
